d59d07f9141edaa01cbc5322832e510f33bd0f48
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-11-19 17:30:57 +0200

    1 v

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { selectContacts } from '../redux/selectors';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import { fetchContacts } from "../redux/operation";
+import FormContact from './Form/Form';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import Section from './Section/Section';
+
 export const App = () => {
+  const contacts = useSelector(selectContacts);
+
+  const dispatch = useDispatch();
+    useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch])
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <Section>
+      <h2>Phonebook</h2>
+      <FormContact/>
+      <Filter/>
+      {contacts.length !== 0 && <ContactList/>}
+    </Section>
+      );
 };
